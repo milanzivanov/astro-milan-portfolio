44528e3bc076d584b6fe21779514d2d7b1d981b7
milanzivanov <milan.zivanov@gmail.com>
2024-04-01 22:38:23 +0200

    test

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -20,6 +20,7 @@ document.addEventListener("keydown", (e) => {
 
 // active link
 document.querySelectorAll(".nav-link").forEach((link) => {
+  console.log(link.href, window.location.href);
   if (link.href === window.location.href) {
     link.setAttribute("aria-current", "page");
   }
