398b49a1ede55636995f21b4f70b190d8c984658
milanzivanov <milan.zivanov@gmail.com>
2024-04-03 14:35:03 +0200

    new testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://milanzivanov.github.io"
+  site: "https://milanzivanov.github.io",
+  redirects: {
+    "/": "",
+    "/work/": "work",
+    "/about/": " about"
+  }
 });
