c78eb544d04a95860023be58d21e4aadcfae2ef7
milanzivanov <milan.zivanov@gmail.com>
2024-04-04 22:41:57 +0200

    in progress

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,19 +1,21 @@
-// hamburger menu
+/////////////////////
+// mobile menu
 document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".line--middle").classList.toggle("active");
-
-  document.querySelector(".line--top").classList.toggle("active");
-
-  document.querySelector(".line--bottom").classList.toggle("active");
+  // hamburger menu
+  // document.querySelector(".line--middle").classList.toggle("active");
+  // document.querySelector(".line--top").classList.toggle("active");
+  // document.querySelector(".line--bottom").classList.toggle("active");
 
   document.querySelector(".nav-links__mobile").classList.toggle("open");
 });
 
+/////////////////////////////////
+// Remove active state from mobile menu with key escape
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
-    document.querySelector(".nav-links__mobile").classList.remove("active");
     document
-      .querySelector(".line--middle, .line--top, .nav-links__mobile")
+      .querySelector(".line--bottom, .line--top, .line--middle")
       .classList.remove("active");
+    document.querySelector(".nav-links__mobile").classList.remove("open");
   }
 });
